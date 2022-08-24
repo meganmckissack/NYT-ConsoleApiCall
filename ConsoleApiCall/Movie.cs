fbc88a87c5f321a0a07fdca228a96814ee690775
Megan McKissack <meganmckissack@gmail.com>
2022-08-23 20:56:00 -0700

    create console app for api call from nyt

---
+++ ConsoleApiCall/Movie.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace ConsoleApiCall.Models
+{
+  public class Movie 
+  {
+    public string Display_Title { get; set; }
+    public string Mpaa_Rating { get; set; }
+    public string Headline { get; set; }
+    public string Summary_Short { get; set; }
+  }
+}
---
+++ ConsoleApiCall/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using RestSharp;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using ConsoleApiCall.Models;
+
+namespace ConsoleApiCall
+{
+    class Program
+    {
+        static void Main()
+        {
+          var apiCallTask = ApiHelper.ApiCall(EnvironmentVariables.ApiKey);
+          var result = apiCallTask.Result;
+
+          JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
+          List<Movie> movieList = JsonConvert.DeserializeObject<List<Movie>>(jsonResponse["results"].ToString());
+
+          foreach (Movie movie in movieList)
+            {
+                Console.WriteLine($"Title: {movie.Display_Title}");
+                Console.WriteLine($"Rating: {movie.Mpaa_Rating}");
+                Console.WriteLine($"Headline: {movie.Headline}");
+                Console.WriteLine($"Summary: {movie.Summary_Short}");
+            }
+
+        }
+    }
+
+    class ApiHelper
+    {
+      public static async Task<string> ApiCall(string apiKey)
+      {
+        RestClient client = new RestClient("https://api.nytimes.com/svc/movies/v2");
+        RestRequest request = new RestRequest($"reviews/picks.json?api-key={apiKey}", Method.GET);
+        var response = await client.ExecuteTaskAsync(request);
+        return response.Content;
+      }
+    }
+}
